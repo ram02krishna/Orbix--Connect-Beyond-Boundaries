45bde2f39217d89688a3a96447e4849435883afc
Ram Krishna <krishnarammhd@gmail.com>
2026-07-24 17:42:19 +0530

    Fix Prisma client adapter initialization

--- server/src/config/prisma.ts
+++ server/src/config/prisma.ts
@@ -1,6 +1,10 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import pg from "pg";
 import { env } from "./env.js";
 
+const pool = new pg.Pool({ connectionString: env.DATABASE_URL });
+const adapter = new PrismaPg(pool);
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
@@ -8,6 +12,7 @@ const globalForPrisma = globalThis as unknown as {
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
+    adapter,
     log:
       env.NODE_ENV === "development"
         ? ["query", "info", "warn", "error"]
